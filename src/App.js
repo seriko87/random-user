1715e813af466201c7cfc87e8bd5c0229ced17be
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-03-02 01:48:49 -0500

    created-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
