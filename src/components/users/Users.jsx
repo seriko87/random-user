8c227aa43f8686a68ceed4c08d2bceb25029ad10
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-03-02 06:02:23 -0500

    dashboard update

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Users = () => {
-  return <div>Users</div>;
+  return <div className="user-container"></div>;
 };
 
 export default Users;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import './dash.css';
 import List from '../../components/list/List';
 import { menuItems } from './menuItems';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import Users from '../../components/users/Users';
+import { Outlet } from 'react-router-dom';
+import DarkModeToggle from '../../components/darkMode/DarkModeToggle';
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState('users');
   return (
     <div className="dashboard-container">
       <nav className="side-nav">
-        <div className="logo">Friendly</div>
+        <div className="logo" style={{ fontSize: '36px' }}>
+          Dashboard
+        </div>
         <div className="list-wrap">
           {menuItems.map((item) => {
             return (
@@ -24,6 +26,9 @@ const Dashboard = () => {
         </div>
       </nav>
       <div className="dash-content">
+        <span>
+          <DarkModeToggle />
+        </span>
         <Outlet />
       </div>
     </div>
